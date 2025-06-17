b93928c3b8c0345bb6eab688a6532681a87af2b8
Matheus Prudente <matheusprutrevigno292@gmail.com>
2025-06-17 19:31:59 -0300

    Update

--- projeto/carregarConteudo.js
+++ projeto/carregarConteudo.js
@@ -19,4 +19,36 @@ function carregarConteudo() {
         });
 }
 
+let minha_biblioteca = JSON.parse(localStorage.getItem("minha_biblioteca")) || [];
 
+function carregarJogosTT() {
+    let jogosTT = JSON.parse(localStorage.getItem("minha_biblioteca")) || [];
+    let container = document.getElementById("listaJogosTT");
+    let aviso = document.querySelector('.aviso');
+    container.innerHTML = '';
+
+    if (jogosTT.length === 0) {
+        aviso.style.display = '';
+    } else {
+        aviso.style.display = 'none';
+        jogosTT.forEach(jogo => {
+            let section = document.createElement("section");
+            section.classList.add("col", "jogo", "item2");
+            section.innerHTML = `
+                <div class="card shadow-sm">
+                    <img src="${jogo.capa}" alt="foto-${jogo.nome}">
+                    <h2 class="fw-normal text-center">${jogo.nome}</h2>
+                    <div class="card-body">
+                        <p class="card-text">${jogo.sobre}.</p>
+                        <label class="form-label">Avaliação</label>
+                        <div class="classificacao" style="display: flex;flex-direction: row;justify-content: flex-start;">
+                            ${[1,2,3,4,5].map(i => `<span class="estrela ${i <= parseInt(jogo.avaliacao) ? "selecionada" : "naoselecionada"}">&#9733;</span>`).join('')}
+                        </div>
+                    </div>
+                </div>
+            `;
+            container.appendChild(section);
+        });
+    }
+}
+window.onload = carregarJogosTT;
